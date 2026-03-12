2adbe9be43d8b4ac2fe1197a96d5b5d38434fe5b
jjean-v <14jean04@gmail.com>
2026-03-12 16:05:08 +0100

    add: course robotics 12-03

--- update-git.sh
+++ update-git.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
-git checkout -B course/12-03
+if [ -z "$1" ]; then
+   echo "Usage: $0 <course-name>"
+   exit 1
+elif [ -z "$2" ]; then
+    mode="add"
+else
+    mode="$2"
+fi
+
+date=$(date +%Y-%m-%d)
+
+git checkout -B course/$date
 
 git add .
 
-git commit -m "course computer security 12-03"
+git commit -m "$mode: course $1 $date"
 
-git push --set-upstream origin course/12-03
-
-
+git push --set-upstream origin course/$date
